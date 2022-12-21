7120fecdd3762caee80a5ff5cf38b1a457a699ca
magdalena11072002 <magdalenajurkowska02@gmail.com>
2022-12-21 17:29:35 +0100

    new commit

--- src/main/java/org/example/GUI/GameWindow.java
+++ src/main/java/org/example/GUI/GameWindow.java
@@ -16,59 +16,58 @@ import java.util.ArrayList;
 
 public class GameWindow extends Application {
     private static Date date;
+    private static int size=10;
 
+
+
+    private static Stage stage = new Stage();
+    private static BorderPane root = new BorderPane();
          //usunac/zmienic/nadawac na biezaco
         static ArrayList<Rectangle> rectangles=new ArrayList<>();
 
-        public static void display() {
-
-           // int size =date.getSize() ; todo naprawic blad NPE
-            int size =10;
-
-            Stage stage = new Stage();
-
-            //layout
-            BorderPane root = new BorderPane(); //zeby mozna bylo pozniej cos dodac innego jeszcze np z boku
+        private static void setup(){
+             //zeby mozna bylo pozniej cos dodac innego jeszcze np z boku
             root.setBackground(new Background(new BackgroundFill(Color.BLANCHEDALMOND, null, null)));
             root.setPadding(new Insets(25)); //marginesy
-
-            GridPane board = new GridPane();//do samej planszy
-            //wypelnianie root
-            root.setCenter(board);
-            root.setLeft(null);
-            root.setRight(null);
-            root.setBottom(null);
-
-            //mozna zrobic zeby zmieniać jakieś polecenia itd
             Label communication = new Label("Rozpoczęto rozgrywke");
             root.setTop(communication);
+            stage.setTitle("Warcaby - rozgrywka");
+        }
 
-            //zeby byla jakas zawartosc
-            //dodaje kwadraty udajace plansze - prawdopdobnie dodac klasy i pokombinować z StackPane
+        public static void display() {
+            setup();
+
+            //w logice
             for(int i = 0; i < size; i++) {
                 for(int j = 0; j< size; j++){
-                    RectangleWithPiece square = new RectangleWithPiece(50,50); // klasa rozszerzona o bierke
-                    Rectangle rectangle = square.getSquare();
-                    rectangles.add(rectangle);
-                    if(((i+j)%2) ==0){
-                        rectangle.setFill(Color.GREY);//byle jaki kolor dalam, byle rozne byly
-                    }
-                    board.add(rectangle, i+1,j+1);
-
-                    //todo dodac do roznych trybow gry pionki przyklad dla standardu, powiazac z sizem i trybem gry - jeszcze nie dodany cyba
                     if((j<2) && (rectangle.getFill()==Color.GREY) ){
                         board.add(square.createPawn("#000000"), i+1,j+1);
                     }
                     else if ((j>7) && (rectangle.getFill()==Color.GREY))
                     board.add(square.createPawn("#FFFFFF"), i+1,j+1);
                 }
+            }
+        }
 
+        public static void refreshDisplay(RectangleWithPiece chessBoard[][]){
+            GridPane board = new GridPane();//do samej planszy
+            root.setCenter(board);
+
+            for(int i=0; i<chessBoard.length;i++){
+                for(int j=0;j<chessBoard[i].length;j++){
+                    RectangleWithPiece square = new RectangleWithPiece(50,50); // klasa rozszerzona o bierke
+                    Rectangle rectangle = square.getSquare();
+                    if(((i+j)%2) ==0){
+                        rectangle.setFill(Color.GREY);//byle jaki kolor dalam, byle rozne byly
+                    }
+                    board.add(rectangle, i+1,j+1);
+                     if(square.getPiece()!=null){
+                         board.add(square.getPiece(),i+1,j+1);
+                     }
+                }
             }
 
-            //dzialanie okna
-            Scene scene = new Scene(root);
-            stage.setTitle("Warcaby - rozgrywka");
-            stage.setScene(scene);
+            stage.setScene(new Scene(root));
             stage.sizeToScene(); //dopasowuje okno do zawartosci
             stage.show();
         }
